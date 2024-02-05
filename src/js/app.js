5c12e114b7169e65646d0e46ab01be73bd382519
msvanaas <31248055+MikaelSvanasbakken@users.noreply.github.com>
2024-02-05 11:59:34 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,3 @@
-//  1. Vi ønsker å vise en Katte-GIF fordi det skaper ro i sjelen. Derfor ønsker jeg at dere peker på riktig element, og kaller getCat funksjonen.
-//  2. Vi må finne en URL å hente Kattegifs fra, kunne dere lagt inn riktig url? Se om dere finner noe her: https://cataas.com/
-//  3. Vi må sørge for at vi sender variabler riktig og peker på riktig navn, kan dere ordne det?
-//  4. Om alt er gjort riktig så vil dere legge merke til at det er mangel på tilbakemelding til brukeren at den laster inn en ny GIF, kan dere ordne det? Spinner, tekst som sier "Laster inn ny GIF...". Sky is the limit.
-//  5. Knappen er ikke veldig pen, kunne dere gjort den penere?
-
-
-
 document.getElementById('').addEventListener('click', () => {
     
 })
